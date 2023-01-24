09a74a0cae5653144155f82281eb245eaedbca9b
andresfgiraldo <andresfgiraldo@live.com>
2023-01-24 09:58:34 -0500

    implement swagger (#2)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, HttpCode, Logger } from '@nestjs/common';
 import { version } from '../package.json';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('healthcheck')
 @Controller()
 export class AppController {
   private readonly logger = new Logger(AppController.name);
 
   @Get('/healthcheck')
+  @ApiOperation({ summary: 'Validar estado general del API' })
   @HttpCode(200)
   async check() {
     this.logger.debug(`healthcheck on ${Date.now()}`);
--- src/inmueble/inmueble.controller.ts
+++ src/inmueble/inmueble.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe, UseInterceptors } from '@nestjs/common';
 import { InmuebleService } from './inmueble.service';
 import { CreateInmuebleDto } from './dto/create-inmueble.dto';
 import { UpdateInmuebleDto } from './dto/update-inmueble.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ValidationExceptionInterceptor } from 'src/common/interceptors/validation.exception.interceptor';
+import { ResponseInterceptor } from 'src/common/interceptors/response.interceptor';
 
+@ApiTags('imbueble')
 @Controller('inmueble')
 @UsePipes(
   new ValidationPipe({
@@ -15,30 +19,36 @@ import { UpdateInmuebleDto } from './dto/update-inmueble.dto';
     errorHttpStatusCode: 422,
   }),
 )
+@UseInterceptors(ValidationExceptionInterceptor, ResponseInterceptor)
 export class InmuebleController {
   constructor(private readonly inmuebleService: InmuebleService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Listar todos los inmuebles' })
   findAll() {
     return this.inmuebleService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Buscar un imbueble por su id' })
   findOne(@Param('id') id: string) {
     return this.inmuebleService.findOne(id);
   }
 
   @Post()
+  @ApiOperation({ summary: 'Crear un imbueble' })
   create(@Body() createInmuebleDto: CreateInmuebleDto) {
     return this.inmuebleService.create(createInmuebleDto);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar un imbueble' })
   update(@Param('id') id: string, @Body() updateInmuebleDto: UpdateInmuebleDto) {
     return this.inmuebleService.update(id, updateInmuebleDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar un imbueble' })
   remove(@Param('id') id: string) {
     return this.inmuebleService.remove(id);
   }
